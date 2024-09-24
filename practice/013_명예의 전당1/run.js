6449438e4a25bc010858c73307be44eba18b3d4b
heeju <bbbb412@naver.com>
2024-09-24 14:08:17 +0900

    240924

--- practice/013_명예의 전당1/run.js
+++ practice/013_명예의 전당1/run.js
@@ -3,7 +3,7 @@
 // k	score	result
 // 3	[10, 100, 20, 150, 1, 100, 200]	[10, 10, 10, 20, 20, 100, 100]
 // 4	[0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]	[0, 0, 0, 0, 20, 40, 70, 70, 150, 300]
-console.log(solution(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]	))
+console.log(solution2(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]	))
 
 function solution(k, score) {
   let tmpArr = []
@@ -20,6 +20,21 @@ function solution(k, score) {
     acc.push(tmpArr[0])
     return acc
   }, [])
-  
+
   return tmp;
+}
+
+// 다른사람거 >> shift로 맨앞거 짤라내면서 새값 넣기
+function solution2(k, score) {
+  var answer = [];
+  let temp = []
+  for(let i = 0; i < score.length; i ++) {
+    temp.push(score[i])
+    temp.sort((a, b) => a - b)
+    if (temp.length > k) {
+      temp.shift()
+    }
+    answer.push(temp[0])
+  }
+  return answer;
 }
