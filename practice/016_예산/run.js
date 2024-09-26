ee2c068a6589efcdf740c409be3d0eec1ab675d2
heeju <bbbb412@naver.com>
2024-09-26 10:52:00 +0900

    240926

---
+++ practice/016_예산/run.js
@@ -0,0 +1,18 @@
+// d	budget	result
+// [1,3,2,5,4]	9	3
+// [2,2,3,3]	10	4
+
+console.log(solution([1,3,2,5,4], 9))
+
+function solution(d, budget) {
+  d.sort((a, b) => a - b)
+  let tmp = 0
+  let index = 0
+  let answer = 0
+  while ((tmp + d[index]) <= budget) {
+    tmp += d[index]
+    answer++
+    index++
+  }
+  return answer;
+}
