59fd84284a4031bccc283d224fd61e7727ce56b8
heeju <bbbb412@naver.com>
2024-10-24 13:07:08 +0900

    241024

---
+++ practice/029_대충 만든 자판/run.js
@@ -0,0 +1,28 @@
+// keymap	targets	result
+// ["ABACD", "BCEFD"]	["ABCD","AABB"]	[9, 4]
+// ["AA"]	["B"]	[-1]
+// ["AGZ", "BSSS"]	["ASA","BGZ"]	[4, 6]
+
+
+console.log(solution(["ABACD", "BCEFD"],	["ABCD","AABB"]))
+
+function solution(keymap, targets) {
+  const json = {}
+  keymap.forEach(ele => {
+    ele.split('').forEach((char, index) => {
+      json[char] = json[char] < index + 1 ? json[char] : index + 1
+    })
+  })
+
+
+  const answer = targets.reduce((acc, ele) => {
+    const sum = ele.split('').reduce((acc, char) => {
+      acc += json[char]
+      return acc
+    }, 0)
+    acc.push(sum > 0 ? sum : -1)
+    return acc
+  }, [])
+
+  return answer;
+}
